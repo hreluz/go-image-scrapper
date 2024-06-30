f07b164598ed7fed93b26f2323f4c1d866ca5245
Hector <hmreluz@gmail.com>
2024-06-29 23:11:34 -0500

    Refactor on output to reuse iteration

--- internal/interaction/output.go
+++ internal/interaction/output.go
@@ -2,25 +2,47 @@ package interaction
 
 import (
 	"fmt"
+	"reflect"
 
 	"github.com/hreluz/images-scrapper/pkg/html_processer/selector"
 	"github.com/hreluz/images-scrapper/pkg/html_processer/tag"
 )
 
 func ShowTagOptions(s string) {
-	fmt.Printf("Choose a tag %s:\n", s)
-	fmt.Println("------------------------------------")
-
-	for i, option := range tag.TAGS_OPTIONS {
-		fmt.Printf("(%d) %s\n", (i + 1), option)
-	}
+	showOptions(tag.TAGS_OPTIONS, fmt.Sprintf("Choose a tag %s", s))
 }
 
 func ShowSelectorOptions(s string) {
-	fmt.Printf("Choose a selector %s:\n", s)
+	showOptions(selector.SELECTOR_TYPE_OPTIONS, fmt.Sprintf("Choose a selector %s", s))
+}
+
+func convertToStringSlice(v interface{}) []string {
+	var options []string
+	rv := reflect.ValueOf(v)
+	for i := 0; i < rv.Len(); i++ {
+		options = append(options, rv.Index(i).String())
+	}
+	return options
+}
+
+func showOptions(v interface{}, message string) {
+	fmt.Printf("%v:\n ", message)
 	fmt.Println("------------------------------------")
 
-	for i, option := range selector.SELECTOR_TYPE_OPTIONS {
+	var options []string
+	switch v := v.(type) {
+	case tag.TagNames:
+		options = convertToStringSlice(v)
+	case selector.SelectorTypes:
+		options = convertToStringSlice(v)
+	case []string:
+		options = v
+	default:
+		fmt.Println("Unsupported type")
+		return
+	}
+
+	for i, option := range options {
 		fmt.Printf("(%d) %s\n", (i + 1), option)
 	}
 }
